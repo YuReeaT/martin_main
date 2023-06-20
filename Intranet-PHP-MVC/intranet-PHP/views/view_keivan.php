cc022d4c21c9a26cc6b42386b11f0d76b8ace993
YuReeaT <60martin.mendoza@gmail.com>
2023-06-20 16:12:14 +0200

    add view keivan

---
+++ Intranet-PHP-MVC/intranet-PHP/views/view_keivan.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8">
+   <title>BTS-blanc</title>
+   <link rel="stylesheet" href="./views/public/css/style.css">
+</head>
+
+<body>
+   <?php include_once("./views/include/header.php") ?>
+   <?php include_once("./views/include/nav.php") ?>
+
+   <main>
+     <h2>blague.issou.ikek.exe</h2>
+       <p>
+            Quel est le comble pour un électricien ?
+            Ne pas être au courant !
+
+            Pourquoi est-ce que les plongeurs plongent toujours en arrière dans l'eau ?
+            Parce que sinon, ils tombent dans le bateau !
+
+            Qu'est-ce qu'un oiseau qui a peur de l'eau ?
+            Un canardéphobe !
+
+            Pourquoi est-ce que Napoléon n'a jamais déménagé ?
+            Parce qu'il déteste Waterloo !
+
+            Qu'est-ce qu'un chien avec une perruque ?
+            Un toutoupe !
+
+            J'espère que ces blagues te feront sourire !  
+      </p>
+   </main>
+
+   <?php include_once("./views/include/footer.php") ?>
+</body>
+
+</html>
